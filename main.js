6e9547f77c72c3ca3c211394d2d18e50d65a9e49
Guneesh922 <95079208+Guneesh922@users.noreply.github.com>
2022-06-10 16:52:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name = "Guneesh";
+                var last_name = "Gupta";
+
+                var full_name = first_name.concat(last_name);
+                console.log(full_name);
+
+                var a = 10;
+                var b = 5;
+
+                var add = a + b;
+                console.log(add);
+
+                var sub = a - b;
+                console.log(sub);
+
+                var multiple = a * b;
+                console.log(multiple);
+
+                var divide = a / b;
+                console.log(divide);
